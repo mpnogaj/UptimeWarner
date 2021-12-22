8003a06a2432c1fa497ac95e633bde97350cea43
mpnogaj <mpnogaj@gmail.com>
2021-12-22 20:34:53 +0100

    Added flashing icon. Implemented own timer

--- UptimeWarner/AppContext.cs
+++ UptimeWarner/AppContext.cs
@@ -1,8 +1,8 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using System.Runtime.InteropServices;
 using System.Windows.Forms;
-using TTimer = System.Threading.Timer;
 using UptimeWarner.Properties;
 
 namespace UptimeWarner
@@ -10,9 +10,14 @@ namespace UptimeWarner
     public class AppContext : ApplicationContext
     {
         private readonly NotifyIcon _notifyIcon;
-        private readonly TTimer _timer;
+        private readonly Timer _timer;
+        private readonly Timer _iconCycleTimer;
+        private readonly TimeSpan _warningTime = TimeSpan.FromDays(1);
+        private readonly TimeSpan _criticalTime = TimeSpan.FromDays(2);
+        private bool _isRedIcon;
         public AppContext()
         {
+            Debug.Assert(_warningTime <= _criticalTime);
             // ReSharper disable once JoinDeclarationAndInitializer
             // ReSharper disable once RedundantAssignment
             var isDebug = false;
@@ -32,21 +37,43 @@ namespace UptimeWarner
                 Visible = true,
                 Icon = Resources.Icon
             };
-            _timer = new TTimer((_) =>
+
+            _iconCycleTimer = new Timer
             {
-                var uptime = GetUptime();
-                Console.WriteLine(TimeSpanToString(uptime));
-                // ReSharper disable once ConditionIsAlwaysTrueOrFalse
-                if (isDebug)
+                Callback = (_) =>
                 {
-                    _notifyIcon.ContextMenu.MenuItems[0]
-                        .Text = string.Format(Resources.uptimeText,
-                        TimeSpanToString(uptime));
-                }
-                _notifyIcon.Icon = ControlCheck(uptime) ? Resources.RedIcon : Resources.GreenIcon;
-                _notifyIcon.Text = ControlCheck(uptime) ? Resources.restartRequired : string.Empty;
+                    _notifyIcon.Icon = _isRedIcon ? Resources.GrayIcon : Resources.RedIcon;
+                    _isRedIcon = !_isRedIcon;
+                },
+                DeltaTime = TimeSpan.FromSeconds(1)
+            };
+            
+            _timer = new Timer
+            {
+                Callback = (sender) =>
+                {
+                    var uptime = GetUptime();
+                    // ReSharper disable once ConditionIsAlwaysTrueOrFalse
+                    if (isDebug) Debug.WriteLine(TimeSpanToString(uptime));
+                    // ReSharper disable once ConditionIsAlwaysTrueOrFalse
+                    if (isDebug)
+                    {
+                        _notifyIcon.ContextMenu.MenuItems[0]
+                            .Text = string.Format(Resources.uptimeText,
+                            TimeSpanToString(uptime));
+                    }
+
+                    _notifyIcon.Icon = IsWarning(uptime) ? Resources.RedIcon : Resources.GreenIcon;
+                    _notifyIcon.Text = IsWarning(uptime) ? Resources.restartRequired : string.Empty;
+
+                    if (!IsCritical(uptime)) return;
+                    sender.Destroy();
+                    _iconCycleTimer.Start();
+                },
                 // ReSharper disable once ConditionIsAlwaysTrueOrFalse
-            }, new object(), 0, isDebug ? 1000 : 1000 * 60 * 60);
+                DeltaTime = isDebug ? TimeSpan.FromSeconds(1) : TimeSpan.FromHours(1)
+            };
+            _timer.Start();
         }
 
         // ReSharper disable once UnusedMember.Local
@@ -54,19 +81,26 @@ namespace UptimeWarner
         private void AppExit(object sender, EventArgs e)
         {
             _notifyIcon.Visible = false;
+            _timer.Destroy();
             _timer.Dispose();
+            _iconCycleTimer.Destroy();
+            _iconCycleTimer.Destroy();
             Application.Exit();
         }
 
-        private static bool ControlCheck(TimeSpan uptime)
+        private bool IsWarning(TimeSpan uptime)
         {
-	        return uptime > TimeSpan.FromDays(1);
-	        //return uptime > TimeSpan.FromMilliseconds(1);
+            return uptime > _warningTime;
+        }
+
+        private bool IsCritical(TimeSpan uptime)
+        {
+            return uptime > _criticalTime;
         }
 
         private static string TimeSpanToString(TimeSpan ts)
         {
-	        return $"{(ts.Days * 24 + ts.Hours):D2}:{ts.Minutes:D2}:{ts.Seconds:D2}";
+            return $"{(ts.Days * 24 + ts.Hours):D2}:{ts.Minutes:D2}:{ts.Seconds:D2}";
         }
 
         /// <summary>
@@ -77,7 +111,7 @@ namespace UptimeWarner
         {
             return TimeSpan.FromMilliseconds(GetTickCount64());
         }
-        
+
         [DllImport("kernel32")]
         private static extern ulong GetTickCount64();
     }
---
+++ UptimeWarner/Timer.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Threading;
+
+namespace UptimeWarner
+{
+    public class Timer : IDisposable
+    {
+        private System.Threading.Timer _timer;
+        private bool _isRunning;
+        private TimeSpan _deltaTime;
+        private Action<Timer> _callback;
+
+        public TimeSpan DeltaTime
+        {
+            private get => _deltaTime;
+            set
+            {
+                if (_isRunning) throw new TimerException();
+                _deltaTime = value;
+            }
+        }
+
+        public Action<Timer> Callback
+        {
+            private get => _callback;
+            set
+            {
+                if (_isRunning) throw new TimerException();
+                _callback = value;
+            }
+        }
+
+        public Timer() { }
+        public Timer(Action<Timer> callback, TimeSpan deltaTime, bool shouldRunInstantly)
+        {
+            DeltaTime = deltaTime;
+            Callback = callback;
+            if(shouldRunInstantly) Start();
+        }
+        public void Start()
+        {
+            _timer = new System.Threading.Timer((_) => Callback(this), new object(), 0, (int)DeltaTime.TotalMilliseconds);
+            _isRunning = true;
+        }
+
+        public void Pause()
+        {
+            _timer.Change(Timeout.Infinite, Timeout.Infinite);
+            _isRunning = false;
+        }
+
+        public void Resume()
+        {
+            _timer.Change(0, (int)DeltaTime.TotalMilliseconds);
+            _isRunning = true;
+        }
+
+        public void Destroy()
+        {
+            _timer.Dispose();
+            _timer = null;
+            _isRunning = false;
+        }
+
+        public void Dispose()
+        {
+            _timer?.Dispose();
+        }
+    }
+
+    public class TimerException : Exception
+    {
+        public override string Message { get; }
+        public TimerException()
+        {
+            Message = "Cannot change delta time or callback for running task!";
+        }
+    }
+}
